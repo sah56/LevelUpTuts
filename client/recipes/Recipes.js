54df80e9778a6c4126e53dc02edce66d142fb3b4
sah56 <hayes.scott56@gmail.com>
2015-12-30 02:46:16 +0000

    this was initiated at lesson 12

---
+++ client/recipes/Recipes.js
@@ -0,0 +1,8 @@
+Meteor.subscribe('recipes');
+//console.log(Meteor.settings.public.ga.account);
+
+Template.Recipes.helpers({
+	recipes:()=> {
+	return Recipes.find({});	
+	}
+});
---
+++ i.js
@@ -0,0 +1,8 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+Session.set("variable_one","Pete");
+}
+
+if (Meteor.isServer) {
+ 
+}
